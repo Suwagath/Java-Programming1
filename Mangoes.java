b79c35bc600e1dd2c9e8207a8eb381b968175ccd
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-04-24 13:03:14 +0530

    Add files via upload

---
+++ Mangoes.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Mangoes {
+    public static void main(String args[]){
+        Scanner tuna = new Scanner(System.in);
+        System.out.println(tuna.nextLine());
+    }
+}
