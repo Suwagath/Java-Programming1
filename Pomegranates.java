c0087d0b8772afbdfa19c0142913f54f1829af0d
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-04-24 18:02:39 +0530

    Add files via upload and my fifth Java code instructing Math operators.

---
+++ Pomegranates.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pomegranates {
+    public static void main(String args[]){
+        Scanner tuna = new Scanner(System.in);
+
+        int girls, boys, people;
+        girls = 7;
+        boys = 3;
+        people = girls / boys;
+        System.out.println(people);
+    }
+}
