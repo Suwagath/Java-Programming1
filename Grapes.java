206ea8b939d4adb2928030320366b78c0121d10a
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-04-24 14:39:11 +0530

    Add files via upload,My fourth java code building, Basic calculator

---
+++ Grapes.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Grapes {;
+    public static void main (String args []){;
+        Scanner tuna = new Scanner(System.in);
+        double fnum, snum, answer;
+        System.out.println("Enter first num; ");
+        fnum = tuna.nextDouble();
+        System.out.println("Enter second num; ");
+        snum = tuna.nextDouble();
+        answer = fnum + snum;
+        System.out.println(answer);
+    }
+}
