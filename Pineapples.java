677772a4ef8f52e30c0585946533d02651f5294f
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-04-25 23:49:30 +0530

    My 7th Java code is completed with "If statement".

---
+++ Pineapples.java
@@ -0,0 +1,14 @@
+
+public class Pineapples {
+        public static void main(String args[]) {
+            int test = 6;
+
+            if (test < 9) {
+                System.out.println("yes");
+            }else{
+                System.out.println("this is else");
+
+            }
+
+        }
+}
