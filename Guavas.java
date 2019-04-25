c29af109d056e370a4c756b203840109c68031a8
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-04-25 22:28:31 +0530

    My 6th Java code done with "Increment operators.

---
+++ Guavas.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Guavas{
+    public static void main(String args[]){
+        Scanner tuna = new Scanner(System.in);
+
+        int high = 5;
+        int bass = 18;
+
+        ++high;
+
+        System.out.println(tuna);
+
+    }
+}
+
+
