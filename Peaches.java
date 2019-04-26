0c1cf6af4d224be92b4a843280e1972b3ce558c1
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-04-26 14:13:51 +0530

    My 8th Java code ended with illustrating "Logical operators".

---
+++ Peaches.java
@@ -0,0 +1,12 @@
+public class Peaches {
+    public static void main(String args[]){
+        int boy, girl;
+        boy = 8;
+        girl = 99;
+        if (boy > 10 || girl < 60 ) {
+            System.out.println("you can enter");
+        }else{
+            System.out.println("you can not enter");
+        }
+    }
+}
