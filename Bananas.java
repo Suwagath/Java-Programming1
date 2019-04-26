a326896e348ca7556af5b94c96992d202d32039f
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-04-26 23:10:11 +0530

    My 9th Java code done with "Switch statement".

---
+++ Bananas.java
@@ -0,0 +1,22 @@
+public class Bananas {
+    public static void main(String args[]){
+        int age;
+        age = 7;
+
+        switch (age){
+            case 1:
+                System.out.println("you can crawl");
+                break;
+            case 2:
+                System.out.println("you can talk");
+                break;
+            case 3:
+                System.out.println("you can get in trouble");
+                break;
+                default:
+                    System.out.println("I dont know how old you are");
+                    break;
+
+        }
+    }
+}
