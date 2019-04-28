8d4a3012854073de6c6f99c9b1e69882ccbffab6
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-04-28 16:04:07 +0530

    My 11th Java done with "Multiple classes."

---
+++ Cherries.java
@@ -0,0 +1,8 @@
+
+public class Cherries {
+    public static void main(String args[]) {
+
+        Tuna TunaObject = new Tuna();
+        TunaObject.simpleMessage();
+    }
+}
---
+++ Tuna.java
@@ -0,0 +1,6 @@
+
+public class Tuna {
+    public void simpleMessage(){
+        System.out.println("This is another class");
+    }
+}
