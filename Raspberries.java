2882be390be2b394f37302fdb39a9b83ef51a2f2
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-05-02 23:43:35 +0530

    My 15th Java code done with If Statements.

---
+++ Raspberries.java
@@ -0,0 +1,18 @@
+public class Raspberries {
+    public static void main(String[] args){
+    int age = 60;
+
+    if (age < 50) {
+        System.out.println("You are young");
+    }else{
+        System.out.println("You are old");
+        if (age > 75) {
+            System.out.println("You are Really old");
+        }else{
+            System.out.println("don't worry you aren't really that old");
+        }
+         }
+
+    }
+}
+
