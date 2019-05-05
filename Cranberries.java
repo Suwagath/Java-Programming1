4f00f87b645d504cf9dbc12108c0e5fda83d538c
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-05-05 22:33:35 +0530

    My 21st Java code done using "do while loops".

---
+++ Cranberries.java
@@ -0,0 +1,11 @@
+public class Cranberries {
+    public static void main(String[] args) {
+        int counter = 15;
+
+        do {
+            System.out.println(counter);
+            counter++;
+
+        }while(counter <=10);
+        }
+    }
