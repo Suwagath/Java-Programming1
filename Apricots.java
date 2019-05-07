24ca4678495a135ff70b19135ed5c8f8486002c7
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-05-07 21:39:51 +0530

    My 25th Java code were creating a Array table.

---
+++ Apricots.java
@@ -0,0 +1,10 @@
+public class Apricots {
+    public static void main(String[] args) {
+        System.out.println("Index\tValue");
+        int tuna[]={32,12,18,54,2};
+
+        for(int counter=0;counter<tuna.length;counter++){
+            System.out.println(counter + "\t" + tuna[counter]);
+        }
+    }
+}
