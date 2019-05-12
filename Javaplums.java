1545319127391c537180d0c8db9f5577452477a4
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-05-12 18:44:22 +0530

    My 33rd Java code = Time class

---
+++ Javaplums.java
@@ -0,0 +1,8 @@
+public class Javaplums {
+   public static void main(String[] args){
+       bucky buckyObject = new bucky();
+       System.out.println(buckyObject.toMilitarry());
+       buckyObject.setTime(13,27, 6);
+       System.out.println(buckyObject.toMilitarry());
+   }
+}
