5b4040a3973b7f8d2e7376f54fe73d274f2bfc74
Suwagath <49843678+Suwagath@users.noreply.github.com>
2019-05-25 20:37:47 +0530

    Finished my 34th Java code by illustrating Display regular times.

--- bucky.java
+++ bucky.java
@@ -4,12 +4,18 @@ public class bucky {
     private int second;
 
     public void setTime(int h, int m, int s){
-        hour = ((h>=0 && h<24 ? h : 0));
-        minute = ((m>=0 && m<60 ? m : 0));
-        second = ((s>=0 && s<60 ? s : 0));
+        hour = ((h>=0 && h<24) ? h : 0);
+        minute = ((m>=0 && m<60) ? m : 0);
+        second = ((s>=0 && s<60) ? s : 0);
     }
 
     public String toMilitarry(){
-        return String.format("%02d:%02d:%02d", hour, minute, second);
+        return String.format("%02d:%02d:%02d", hour, minute, second );
     }
+
+    public String toString(){
+        return String.format("%d:%02d:%02d %s", ((hour==0||hour==12)?12:hour%12), minute, second, (hour < 12? "AM": "PM"));
+
+    }
+
 }
---
+++ flora.java
@@ -0,0 +1,11 @@
+public class flora{
+    public static void main(String[] args){
+        bucky buckyObject = new bucky();
+        System.out.println(buckyObject.toMilitarry());
+        System.out.println(buckyObject.toString());
+
+        buckyObject.setTime(13,27,6);
+        System.out.println(buckyObject.toMilitarry());
+        System.out.println(buckyObject.toString());
+    }
+}
